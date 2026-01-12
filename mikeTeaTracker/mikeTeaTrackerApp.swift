83cda4044fb7498b91c9d722196753b1666e5563
Tong Wang <simbinoart@gmail.com>
2026-01-12 16:56:47 -0500

    Initial Commit

---
+++ mikeTeaTracker/mikeTeaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mikeTeaTrackerApp.swift
+//  mikeTeaTracker
+//
+//  Created by Tong Wang on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct mikeTeaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
